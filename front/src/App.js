f569a7c7d1c8b9728d631350a26a0af8efff945c
taewankang <37101468+taewankang@users.noreply.github.com>
2020-08-01 20:37:01 +0900

    add Menu.js
    * modify app.js
    
    * add Menu

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Menu from './page/menu';
 
 function App() {
   return (
     <div>
+      <Menu/>
     </div>
   );
 }
---
+++ front/src/page/menu/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MenuContainer = styled.div`
+  width: 100%;
+`
+
+const Menu = () => {
+    return (
+        <MenuContainer>
+          
+        </MenuContainer>
+    )
+}
+
+export default Menu;
