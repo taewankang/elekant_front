193e01be36ffb6b79005497fbc9b32c47289bd85
leesangha <sanghalee1@naver.com>
2020-08-04 14:08:59 +0900

    Update SignUpContent.js

--- front/src/component/signUp/SignUpContent.js
+++ front/src/component/signUp/SignUpContent.js
@@ -1,11 +1,82 @@
-import React from 'react';
+import React from "react";
+import {
+  Title,
+  InputContainer,
+  SignUpContainer,
+  SignUpContents,
+} from "./style";
+import { Form, Input, Button, Menu, Drowdown, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="http://www.alipay.com/"
+      >
+        1st menu item
+      </a>
+    </Menu.Item>
+  </Menu>
+);
 
 const SignUpContent = () => {
   return (
     <div>
-      회원가입 하기 위한 화면
-    </div>
-  )
-}
+      <SignUpContainer>
+        <SignUpContents>
+          <Title>회원가입</Title>
+          <InputContainer>
+            <Form
+              style={{ width: "80%" }}
+              name="basic"
+              initialValues={{ remember: true }}
+            >
+              <Form.Item
+                name="username"
+                rules={[{ required: true, message: "아이디를 입력하세요!" }]}
+              >
+                <Input placeholder="아이디" />
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password placeholder="비밀번호" />
+              </Form.Item>
+              <Form.Item name="password_check">
+                <Input.Password placeholder="비밀번호 확인" />
+              </Form.Item>
+              <Form.Item name="school">
+                <Input placeholder="학교 찾기" /> <Button>asdf</Button>
+              </Form.Item>
 
-export default SignUpContent
+              <Form.Item name="mail">
+                <Input placeholder="mail" />
+              </Form.Item>
+              <Dropdown overlay={menu}>
+                <a
+                  className="ant-dropdown-link"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Hover me <DownOutlined />
+                </a>
+              </Dropdown>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Form.Item>
+                  <Button
+                    style={{ fontWeight: "900", backgroundColor: "#C62917" }}
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    Submit
+                  </Button>
+                </Form.Item>
+              </div>
+            </Form>
+          </InputContainer>
+        </SignUpContents>
+      </SignUpContainer>
+    </div>
+  );
+};
+
+export default SignUpContent;
