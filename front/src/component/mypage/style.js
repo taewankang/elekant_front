f6f7d238a585b8c79eb420a7e610f312d62aeb55
taewankang <37101468+taewankang@users.noreply.github.com>
2020-08-04 22:21:53 +0900

    modify my page

---
+++ front/src/component/mypage/style.js
@@ -0,0 +1,27 @@
+import {MY_PAGE_BORDER} from '../../color';
+import styled from 'styled-components';
+export const Block = styled.div`
+    width: 700px;
+    height: 70px;
+    padding: 20px;
+    display: flex;
+    background-color: #FFFFFF;
+    border: 1px solid ${MY_PAGE_BORDER};
+`
+
+export const Bold = styled.div`
+    width: 80px;
+    font-weight: 800;
+`
+
+export const Title = styled.div`
+    font-weight: 900;
+    font-size: 20px;
+    width: 700px;
+    height: 70px;
+    padding: 20px;
+    display: flex;
+    background-color: #FFFFFF;
+    color: #000000;
+    border: 1px solid ${MY_PAGE_BORDER};
+`
