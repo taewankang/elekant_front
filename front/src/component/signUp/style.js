4e9aaa22c18866c6e38155feb5fc3c7630a323e9
leesangha <sanghalee1@naver.com>
2020-08-04 14:09:02 +0900

    Create style.js

---
+++ front/src/component/signUp/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  font-family: bold;
+  font-weight: 900;
+  font-size: 25px;
+`;
+export const SignUpContents = styled.div`
+  width: 500px;
+  height: 400px;
+  margin-top: 100px;
+`;
+export const SignUpContainer = styled.div`
+  width: 100%;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+`;
+export const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  height: 100%;
+`;
