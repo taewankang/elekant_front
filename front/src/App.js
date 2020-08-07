0f3ad530aea92de313d2ce95ae2470aa803fa86a
leesangha <sanghalee1@naver.com>
2020-08-07 17:46:46 +0900

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -5,7 +5,7 @@ import Home from "./page/Home";
 import { useDispatch, useSelector } from "react-redux";
 import SignUp from "./page/SignUp";
 import Login from "./page/Login";
-import Mento from "./page/Mento";
+import Mento from "./page/Mentor";
 import Mentee from "./page/Mentee";
 import Board from "./page/Board";
 import MyPage from "./page/MyPage";
@@ -20,7 +20,7 @@ const App = () => {
           <Route exact path="/" component={Home} /> //메인 페이지
           <Route path="/signup" component={SignUp} /> //회원가입 페이지
           <Route path="/login" component={Login} /> //로그인 화면
-          <Route exact path="/mento" component={Mento} /> //멘토 페이지
+          <Route exact path="/mentor" component={Mento} /> //멘토 페이지
           <Route path="/mentee" component={Mentee} /> //멘티 페이지
           <Route path="/board" component={Board} /> //게시판 화면
           <Route path="/mypage" component={MyPage} /> //게시판 화면
