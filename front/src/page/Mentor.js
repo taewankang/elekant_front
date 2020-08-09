b659f77da31b8fa8bbf85b75aa49c8ce737866b3
leesangha <sanghalee1@naver.com>
2020-08-10 02:12:41 +0900

    Update Mentor.js

--- front/src/page/Mentor.js
+++ front/src/page/Mentor.js
@@ -2,23 +2,18 @@ import React from "react";
 import styled from "styled-components";
 import { TreeSelect, Button } from "antd";
 import { BUTTON_COLOR } from "../color";
-const MentorInfo = {
-  name: "Lee",
-  comment: `I'm still hungry`,
-  interest: ["#코딩", "#공부"],
-};
+import ProfileBox from "../component/Mentor/ProfileBox";
 const HashBox = styled.span``;
 const Bold = styled.span`
   margin-left: 3%;
   & + & {
-    margin-left: 45%;
+    margin-left: 42%;
   }
 `;
 const Mymentor = styled.div`
   display: flex;
   width: 50%;
   height: inherit;
-  border: 1px solid black;
   align-items: center;
   justify-content: center;
 `;
@@ -27,7 +22,6 @@ const Container = styled.div`
   margin-top: 0.1rem;
   width: 100%;
   height: 30rem;
-  border: 1px solid red;
 `;
 const MentorArea = styled.div`
   display: flex;
@@ -38,7 +32,7 @@ const MentorArea = styled.div`
 `;
 const MenteeBox = styled.div`
   width: 240px;
-  height: 160px;
+  height: 100px;
   border: 1px solid skyblue;
   border-radius: 7px;
   & + & {
@@ -54,29 +48,7 @@ const Mento = () => {
       <Bold>
         <b>추천 멘토</b>
       </Bold>
-      <MenteeBox>
-        <div>
-          <div>
-            <span>{MentorInfo.name}</span>
-            <span>{MentorInfo.interest[0]}</span>
-            <span>{MentorInfo.interest[1]}</span>
-          </div>
-          <div>
-            <span>{MentorInfo.comment}</span>
-            <Button
-              style={{
-                height: 25,
-                fontWeight: 900,
-                background: BUTTON_COLOR,
-                borderRadius: 8,
-              }}
-              type="primary"
-            >
-              추가 정보 보기
-            </Button>
-          </div>
-        </div>
-      </MenteeBox>
+
       <Container>
         <Mymentor>
           <MentorArea>멘토 영역</MentorArea>
@@ -86,6 +58,9 @@ const Mento = () => {
             placeholder="Please select"
             dropdownStyle={{ maxHeight: 300, overflow: "auto" }}
           />
+          <MenteeBox>
+            <ProfileBox />
+          </MenteeBox>
         </Mymentor>
       </Container>
     </div>
