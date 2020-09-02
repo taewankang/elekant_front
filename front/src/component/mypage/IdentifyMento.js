cf9de7117d72a7bf1e9abc8a771bebaf196dcd6b
taewankang <37101468+taewankang@users.noreply.github.com>
2020-09-02 11:08:57 +0900

    IdentifyMento

--- front/src/component/mypage/IdentifyMento.js
+++ front/src/component/mypage/IdentifyMento.js
@@ -1,12 +1,28 @@
 //마이 페이지 - 멘토 인증
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Title, Block, Bold } from './style';
+import { Select } from 'antd';
+const Option = Select.Option;
 
 const IdentifyMento = () => {
-    return (
+  return (
+    <Container>
+      <Title>멘토 인증</Title>
+      <Block>
+        <Bold>멘토 인증</Bold>
+        <input />
+        &nbsp;@ &nbsp;
+        <Select defaultValue="naver.com" style={{ width: 180 }}>
+          <Option value="naver.com">naver.com</Option>
+          <Option value="samsung.com">samsung.com</Option>
+          <Option value="kakao.com">kakao.com</Option>
+        </Select>
         <div>
-          
+          <button>인증</button>
         </div>
-    )
-}
+      </Block>
+    </Container>
+  );
+};
 
-export default IdentifyMento
+export default IdentifyMento;
